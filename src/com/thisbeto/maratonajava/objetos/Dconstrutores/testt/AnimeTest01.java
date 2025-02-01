11fb145b30a568065bea981b02a06941856cb03b
thisbeto <albertohuber6475@gmail.com>
2025-02-01 20:59:48 -0300

    feature: Aula13 - Construtores

---
+++ src/com/thisbeto/maratonajava/objetos/Dconstrutores/testt/AnimeTest01.java
@@ -0,0 +1,10 @@
+package com.thisbeto.maratonajava.objetos.Dconstrutores.testt;
+
+import com.thisbeto.maratonajava.objetos.Dconstrutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Sasuke","TV",12, "Shonen","Disney");
+        anime.imprime();
+    }
+}
