b9e06bf058053cf71e6c55087ea16ca651719dd8
thisbeto <albertohuber6475@gmail.com>
2025-02-10 16:14:04 -0300

    feature: Aula18 - Sobrescrita do método toString

---
+++ src/com/thisbeto/maratonajava/objetos/Isobrescrita/testt/AnimeTest01.java
@@ -0,0 +1,10 @@
+package com.thisbeto.maratonajava.objetos.Isobrescrita.testt;
+
+import com.thisbeto.maratonajava.objetos.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Estranho");
+        System.out.println(anime);
+    }
+}
