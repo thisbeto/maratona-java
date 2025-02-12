c083c23897dbfe4260a012665b2d1cc488856ffd
thisbeto <albertohuber6475@gmail.com>
2025-02-12 00:10:09 -0300

    feature: Aula20 - Enumeração pt4

--- src/com/thisbeto/maratonajava/objetos/Kenum/testt/ClienteTest01.java
+++ src/com/thisbeto/maratonajava/objetos/Kenum/testt/ClienteTest01.java
@@ -14,7 +14,12 @@ public class ClienteTest01 {
         System.out.println(cliente2);
         System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
         System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
-
+        TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
+        // Aqui se deixar diferente ele vai dar errado porque o valueOf retorna no nome
+        System.out.println(tipoCliente);
+        TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Física");
+        // Estamos devolvendo "Pessoa física" que é um atributo e ele vai nos devolver o nome do objeto, que é PESSOA_FISICA
+        System.out.println(tipoCliente2);
 
     }
 }
