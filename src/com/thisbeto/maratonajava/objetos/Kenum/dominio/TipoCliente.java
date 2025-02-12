30b2757e738954e6c2ed7181c428def9b3ca71da
thisbeto <albertohuber6475@gmail.com>
2025-02-11 23:15:15 -0300

    feature: Aula20 - Enumeração pt2

--- src/com/thisbeto/maratonajava/objetos/Kenum/dominio/TipoCliente.java
+++ src/com/thisbeto/maratonajava/objetos/Kenum/dominio/TipoCliente.java
@@ -1,6 +1,18 @@
 package com.thisbeto.maratonajava.objetos.Kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
+
+    public String nomeRelatorio;
+    public final int VALOR;
+
+    TipoCliente (int valor, String nomeRelatorio) {
+        this.VALOR = valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
 }
--- src/com/thisbeto/maratonajava/objetos/Kenum/testt/ClienteTest01.java
+++ src/com/thisbeto/maratonajava/objetos/Kenum/testt/ClienteTest01.java
@@ -5,14 +5,12 @@ import com.thisbeto.maratonajava.objetos.Kenum.dominio.TipoCliente;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Sasuke", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Naruto",TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Sasuke",TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Naruto",TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Sasuke", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Naruto",TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
+
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
+
 
     }
 }
