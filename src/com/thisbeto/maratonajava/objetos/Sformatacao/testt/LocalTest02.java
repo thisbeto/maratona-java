e1bfb7ca60969e7ee3726a9956c8d340d907e5d3
thisbeto <albertohuber6475@gmail.com>
2025-02-27 22:49:10 -0300

    feature: Aula28 - Formatação

---
+++ src/com/thisbeto/maratonajava/objetos/Sformatacao/testt/LocalTest02.java
@@ -0,0 +1,18 @@
+package com.thisbeto.maratonajava.objetos.Sformatacao.testt;
+
+import java.util.Locale;
+
+public class LocalTest02 {
+    public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+        String[] isoCountries = Locale.getISOCountries();
+        String[] isoLanguages = Locale.getISOLanguages();
+        for (String isoLanguage : isoLanguages) {
+            System.out.print(isoLanguage+ " ");
+        }
+        System.out.println();
+        for (String isoCountrie : isoCountries) {
+            System.out.print(isoCountrie+ " ");
+        }
+    }
+}
