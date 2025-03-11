a06806f144cfa0e3a3a1b8248129614a2fe272e3
thisbeto <albertohuber6475@gmail.com>
2025-03-10 22:42:47 -0300

    feature: Aula 31 - File, Buffered, Writters, Readers

---
+++ src/com/thisbeto/maratonajava/objetos/Vio/testt/FileReaderTest01.java
@@ -0,0 +1,19 @@
+package com.thisbeto.maratonajava.objetos.Vio.testt;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderTest01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try(FileReader fr = new FileReader(file)){
+            int i;
+            while ((i=fr.read()) != -1) {
+                System.out.print((char)i);
+            }
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/com/thisbeto/maratonajava/objetos/Vio/testt/FileWriterTest01.java
+++ src/com/thisbeto/maratonajava/objetos/Vio/testt/FileWriterTest01.java
@@ -1,18 +1,24 @@
 package com.thisbeto.maratonajava.objetos.Vio.testt;
 
 // File
-// FileWriter
-// FileReader
-// BufferedWriter
-// BufferedReader
+// FileWriter -> Escrever um arquivo, de uma maneira mais simples
+// FileReader -> Ler arquivo
+// BufferedWriter -> Escrita num arquivo, mas com uma performance e trabalhando com dados em memoria
+// BufferedReader -> Ler arquivos
 
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
-        FileWriter fw = new FileWriter();
+        try (FileWriter fw = new FileWriter(file, true) ){ // Pode passar um nome do arquivo
+            fw.write("O DevDojo é o melhor curso do Brasil!");
+            fw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
